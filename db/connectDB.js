1589c9df5b8785b8b70087eba3f387db47b2c3c5
Rahul Soni <164996532+rageblack12@users.noreply.github.com>
2025-07-09 16:50:49 +0530

    Updated connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/BusinessGenAi');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected`);
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
